f1d4b04370be0b8dd899273901085fdf0716315b
조소영 <ok69531@gmail.com>
2024-04-01 16:45:58 +0900

    reocn-number of init neg

--- dataset/generate_negative.py
+++ dataset/generate_negative.py
@@ -23,12 +23,12 @@ class NegativeSampling(Dataset):
         negative_samples = torch.tensor([])
         while len(negative_samples) < self.negative_sample_size:
             if self.mode == 'head-batch':
-                negative_sample = torch.randint(0, self.entity_dict[head_type], (300,))
+                negative_sample = torch.randint(0, self.entity_dict[head_type], (2000,))
                 mask = torch.all(negative_sample.view(-1, 1) != torch.tensor(self.all_true_head[(relation, tail)]).view(1, -1), dim = 1)
                 negative_samples = torch.cat([negative_sample[mask], negative_samples])
             
             elif self.mode == 'tail-batch':
-                negative_sample = torch.randint(0, self.entity_dict[tail_type], (300,))
+                negative_sample = torch.randint(0, self.entity_dict[tail_type], (2000,))
                 mask = torch.all(negative_sample.view(-1, 1) != torch.tensor(self.all_true_tail[(head, relation)]).view(1, -1), dim = 1)
                 negative_samples = torch.cat([negative_sample[mask], negative_samples])
         
