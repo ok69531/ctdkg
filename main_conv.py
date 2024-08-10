36a5b2a0f9a3f34582cfbd44cbc4619f6c991f4c
조소영 <ok69531@gmail.com>
2024-08-10 14:22:44 +0900

    recon

--- main_conv.py
+++ main_conv.py
@@ -1,5 +1,6 @@
 import os
 # import wandb
+import random
 import logging
 
 import numpy as np
@@ -396,6 +397,15 @@ def main():
     set_seed(args.seed)
     print(f'====================== run: {args.seed} ======================')
 
+    if args.dataset in ['gd', 'cgd', 'cgpd', 'ctd']:
+        idx = random.sample(range(len(train_triples['head'])), int(len(train_triples['head'])*args.train_frac))
+
+        train_triples['head'] = train_triples['head'][idx]
+        train_triples['tail'] = train_triples['tail'][idx]
+        train_triples['relation'] = train_triples['relation'][idx]
+        train_triples['head_type'] = [train_triples['head_type'][i] for i in idx]
+        train_triples['tail_type'] = [train_triples['tail_type'][i] for i in idx]
+    
     train_dataloader_head = DataLoader(
         TrainDataset(train_triples, nentity, nrelation, 
             args.negative_sample_size, 'head-batch',
